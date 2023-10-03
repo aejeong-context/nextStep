20c77dc08954f84831aa06f3aad362a028342bd3
aejeong <aejeong-context@kakao.com>
2023-10-03 17:51:03 +0900

    feat: 문자열 계산기 요구사항 추가
    - 빈문자열, null, 숫자 하나 입력일 경우, (,|:) 이거나 커스텀 구분자일 경우
    - 예외처리 추가

--- src/main/java/com/step/api/StringCalculator.java
+++ src/main/java/com/step/api/StringCalculator.java
@@ -1,12 +1,35 @@
 package com.step.api;
 
 import java.util.Arrays;
+import org.springframework.util.ObjectUtils;
 
 public class StringCalculator {
 
   public int calculator(String stringParam) {
-    int[] numArrayList = getNumArrayList(stringParam);
-    return Arrays.stream(numArrayList).sum();
+    // 빈 문자열 또는 null 값을 입력할 경우 0 반환
+    if (ObjectUtils.isEmpty(stringParam)) {
+      return 0;
+    }
+
+    // 숫자가 하나 입력될 경우 해당 숫자 리턴
+    if (stringParam.length() == 1) {
+      return Integer.parseInt(stringParam);
+    }
+
+    int result = 0;
+    if (stringParam.contains(",") || stringParam.contains(":")) {
+      result = Arrays.stream(stringParam.split(",|:")).mapToInt(Integer::parseInt).sum();
+    }
+
+    if (stringParam.contains("//") && stringParam.contains("\n")) {
+      result = Arrays.stream(getNumArrayList(stringParam)).sum();
+    }
+
+    // 음수를 전달하는 경우 예외처리
+    if (result < 0) {
+      throw new RuntimeException();
+    }
+    return result;
   }
 
   public int[] getNumArrayList(String includedDelimiterString) {
@@ -24,5 +47,4 @@ public class StringCalculator {
         includedDelimiterString.lastIndexOf("/") + 1,
         includedDelimiterString.indexOf('\n'));
   }
-
 }
